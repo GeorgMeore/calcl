af9ef35b014b876b2b53e98ab28973034263385e
GeorgII <georgii.galushko@gmail.com>
2022-11-15 19:17:57 +0300

    better debugging output

--- debug.c
+++ debug.c
@@ -4,40 +4,58 @@
 
 void print_tokens(const Token *tokens)
 {
-	printf("[");
-	while (tokens->type != EOF_TOKEN) {
-		printf("%.*s, ", tokens->length, tokens->string);
-		tokens++;
+	while (tokens++) {
+		switch (tokens->type) {
+			case LPAREN_TOKEN:
+				printf("LPAREN('%.*s')\n", tokens->length, tokens->string);
+				break;
+			case RPAREN_TOKEN:
+				printf("RPAREN('%.*s')\n", tokens->length, tokens->string);
+				break;
+			case ASTERISK_TOKEN:
+				printf("ASTERISK('%.*s')\n", tokens->length, tokens->string);
+				break;
+			case PLUS_TOKEN:
+				printf("PLUS('%.*s')\n", tokens->length, tokens->string);
+				break;
+			case NUMBER_TOKEN:
+				printf("NUMBER('%.*s')\n", tokens->length, tokens->string);
+				break;
+			case EOF_TOKEN:
+				printf("EOF\n");
+				return;
+		}
 	}
-	printf("EOF]\n");
 }
 
-static void print_expression_rec(const Node *expr, int putnl)
+static void indent(int level)
+{
+	while (level--) {
+		putchar('\t');
+	}
+}
+
+static void print_tree(const Node *expr, int level)
 {
 	switch (expr->type) {
 		case NUMBER_NODE:
-			printf("%d", expr->value.number);
+			indent(level); printf("NUMBER: %d\n", expr->value.number);
 			break;
 		case SUM_NODE:
-			printf("(+ ");
-			print_expression_rec(expr->value.left, 0);
-			printf(" ");
-			print_expression_rec(expr->value.right, 0);
-			printf(")");
+			indent(level); printf("SUM: {\n");
+			print_tree(expr->value.left, level + 1);
+			print_tree(expr->value.right, level + 1);
+			indent(level); printf("}\n");
 			break;
 		case PRODUCT_NODE:
-			printf("(*");
-			print_expression_rec(expr->value.left, 0);
-			printf(" ");
-			print_expression_rec(expr->value.right, 0);
-			printf(")");
-	}
-	if (putnl) {
-		printf("\n");
+			indent(level); printf("PRODUCT: {\n");
+			print_tree(expr->value.left, level + 1);
+			print_tree(expr->value.right, level + 1);
+			indent(level); printf("}");
 	}
 }
 
-void print_expression(const Node *expr)
+void print_expr(const Node *expr)
 {
-	print_expression_rec(expr, 1);
+	print_tree(expr, 0);
 }
