884d4ec679523bc8a08ff3e279ce618dbfbf0339
GeorgII <>
2023-09-09 01:31:29 +0300

    refactored the debug-printing code (maybe I should remove it?)

--- debug.c
+++ debug.c
@@ -6,128 +6,129 @@
 #include "node.h"
 
 
-#define do_print_token(name, token) \
-	(printf(name "('%.*s')\n", token.length, token.string))
+#define tokprintf(name, token) \
+	(printf("<%s|'%.*s'>", name, token.length, token.string))
 
-void print_token(Token token)
+void Token_print(Token token)
 {
 	switch (token.type) {
-		case NUMBER_TOKEN:   do_print_token("NUMBER", token);   break;
-		case ID_TOKEN:       do_print_token("ID", token);       break;
-		case LPAREN_TOKEN:   do_print_token("LPAREN", token);   break;
-		case RPAREN_TOKEN:   do_print_token("RPAREN", token);   break;
-		case CARET_TOKEN:    do_print_token("CARET", token);    break;
-		case ASTERISK_TOKEN: do_print_token("ASTERISK", token); break;
-		case SLASH_TOKEN:    do_print_token("SLASH", token);    break;
-		case PERCENT_TOKEN:  do_print_token("PERCENT", token);  break;
-		case PLUS_TOKEN:     do_print_token("PLUS", token);     break;
-		case MINUS_TOKEN:    do_print_token("MINUS", token);    break;
-		case GT_TOKEN:       do_print_token("GT", token);       break;
-		case LT_TOKEN:       do_print_token("LT", token);       break;
-		case EQ_TOKEN:       do_print_token("EQ", token);       break;
-		case AND_TOKEN:      do_print_token("AND", token);      break;
-		case OR_TOKEN:       do_print_token("OR", token);       break;
-		case IF_TOKEN:       do_print_token("IF", token);       break;
-		case THEN_TOKEN:     do_print_token("THEN", token);     break;
-		case ELSE_TOKEN:     do_print_token("ELSE", token);     break;
-		case FN_TOKEN:       do_print_token("FN", token);       break;
-		case COLON_TOKEN:    do_print_token("COLON", token);    break;
-		case LET_TOKEN:      do_print_token("LET", token);      break;
-		case ERROR_TOKEN:    do_print_token("ERROR", token);    break;
-		case END_TOKEN:      printf("END\n");
+		case NUMBER_TOKEN:   tokprintf("number", token);   break;
+		case ID_TOKEN:       tokprintf("id", token);       break;
+		case LPAREN_TOKEN:   tokprintf("lparen", token);   break;
+		case RPAREN_TOKEN:   tokprintf("rparen", token);   break;
+		case CARET_TOKEN:    tokprintf("caret", token);    break;
+		case ASTERISK_TOKEN: tokprintf("asterisk", token); break;
+		case SLASH_TOKEN:    tokprintf("slash", token);    break;
+		case PERCENT_TOKEN:  tokprintf("percent", token);  break;
+		case PLUS_TOKEN:     tokprintf("plus", token);     break;
+		case MINUS_TOKEN:    tokprintf("minus", token);    break;
+		case GT_TOKEN:       tokprintf("gt", token);       break;
+		case LT_TOKEN:       tokprintf("lt", token);       break;
+		case EQ_TOKEN:       tokprintf("eq", token);       break;
+		case AND_TOKEN:      tokprintf("and", token);      break;
+		case OR_TOKEN:       tokprintf("or", token);       break;
+		case IF_TOKEN:       tokprintf("if", token);       break;
+		case THEN_TOKEN:     tokprintf("then", token);     break;
+		case ELSE_TOKEN:     tokprintf("else", token);     break;
+		case FN_TOKEN:       tokprintf("fn", token);       break;
+		case COLON_TOKEN:    tokprintf("colon", token);    break;
+		case LET_TOKEN:      tokprintf("let", token);      break;
+		case ERROR_TOKEN:    tokprintf("error", token);    break;
+		case END_TOKEN:      tokprintf("end", token);      break;
 	}
 }
 
 static void indent(int level)
 {
-	while (level--) {
-		putchar('\t');
+	while (level > 0) {
+		printf("  ");
+		level -= 1;
 	}
 }
 
-static void print_tree(const Node *expr, int level);
+#define iprintln(level, string)\
+	(indent(level), puts(string))
 
-static void print_pair(const Node *expr, const char *type_string, int level)
+#define iprintfln(level, fmt, args...)\
+	(indent(level), printf(fmt "\n", args))
+
+static void Node_iprint(const Node *expr, int level);
+
+static void PairNode_iprint(const Node *expr, const char *type_string, int level)
 {
 	if (PairNode_op(expr)) {
-		indent(level); printf("%s(%c): {\n", type_string, PairNode_op(expr));
+		iprintfln(level, "%s(%c): {", type_string, PairNode_op(expr));
 	} else {
-		indent(level); printf("%s: {\n", type_string);
+		iprintfln(level, "%s: {", type_string);
 	}
-	print_tree(PairNode_left(expr), level + 1);
-	print_tree(PairNode_right(expr), level + 1);
-	indent(level); printf("}\n");
+	Node_iprint(PairNode_left(expr), level + 1);
+	Node_iprint(PairNode_right(expr), level + 1);
+	iprintln(level, "}");
 }
 
-static void print_if(const Node *expr, int level)
+static void IfNode_iprint(const Node *expr, int level)
 {
-	indent(level); printf("IF: {\n");
-	indent(level + 1); printf("CONDITION:\n");
-	print_tree(IfNode_cond(expr), level + 2);
-	indent(level + 1); printf("THEN:\n");
-	print_tree(IfNode_true(expr), level + 2);
-	indent(level + 1); printf("ELSE:\n");
-	print_tree(IfNode_false(expr), level + 2);
-	indent(level); printf("}\n");
+	iprintln(level, "IF: {");
+	iprintln(level + 1, "CONDITION:");
+	Node_iprint(IfNode_cond(expr), level + 2);
+	iprintln(level + 1, "THEN:");
+	Node_iprint(IfNode_true(expr), level + 2);
+	iprintln(level + 1, "ELSE:");
+	Node_iprint(IfNode_false(expr), level + 2);
+	iprintln(level, "}");
 }
 
-static void print_fn(const Node *expr, int level)
+static void FnNode_iprint(const Node *expr, int level)
 {
-	indent(level); printf("FN: {\n");
-	indent(level + 1); printf("PARAM:\n");
-	print_tree(FnNode_param(expr), level + 2);
-	indent(level + 1); printf("BODY:\n");
-	print_tree(FnNode_body(expr), level + 2);
-	indent(level); printf("}\n");
+	iprintln(level, "FN: {");
+	iprintln(level + 1, "PARAM:");
+	Node_iprint(FnNode_param(expr), level + 2);
+	iprintln(level + 1, "BODY:");
+	Node_iprint(FnNode_body(expr), level + 2);
+	iprintln(level, "}");
 }
 
-static void print_let(const Node *expr, int level)
+static void LetNode_iprint(const Node *expr, int level)
 {
-	indent(level); printf("LET: {\n");
-	indent(level + 1); printf("NAME:\n");
-	print_tree(LetNode_name(expr), level + 2);
-	indent(level + 1); printf("VALUE:\n");
-	print_tree(LetNode_value(expr), level + 2);
-	indent(level); printf("}\n");
+	iprintln(level, "LET: {");
+	iprintln(level + 1, "NAME:");
+	Node_iprint(LetNode_name(expr), level + 2);
+	iprintln(level + 1, "VALUE:");
+	Node_iprint(LetNode_value(expr), level + 2);
+	iprintln(level, "}");
 }
 
-static void print_neg(const Node *expr, int level)
+static void NegNode_iprint(const Node *expr, int level)
 {
-	indent(level); printf("NEG: {\n");
-	print_tree(NegNode_value(expr), level + 1);
-	indent(level); printf("}\n");
+	iprintln(level, "NEG: {");
+	Node_iprint(NegNode_value(expr), level + 1);
+	iprintln(level, "}");
 }
 
-static void print_number(const Node *expr, int level)
-{
-	indent(level); printf("NUMBER: %lf\n", NumNode_value(expr));
-}
-
-static void print_id(const Node *expr, int level)
-{
-	indent(level); printf("ID: %s\n", IdNode_value(expr));
-}
-
-static void print_tree(const Node *expr, int level)
+static void Node_iprint(const Node *expr, int level)
 {
 	switch (expr->type) {
-		case NUMBER_NODE:      print_number(expr, level);                 break;
-		case ID_NODE:          print_id(expr, level);                     break;
-		case NEG_NODE:         print_neg(expr, level);                    break;
-		case APPLICATION_NODE: print_pair(expr, "APPLICATION", level);    break;
-		case EXPT_NODE:        print_pair(expr, "EXPONENTIATION", level); break;
-		case PRODUCT_NODE:     print_pair(expr, "PRODUCT", level);        break;
-		case SUM_NODE:         print_pair(expr, "SUM", level);            break;
-		case CMP_NODE:         print_pair(expr, "COMPARISON", level);     break;
-		case AND_NODE:         print_pair(expr, "AND", level);            break;
-		case OR_NODE:          print_pair(expr, "OR", level);             break;
-		case IF_NODE:          print_if(expr, level);                     break;
-		case FN_NODE:          print_fn(expr, level);                     break;
-		case LET_NODE:         print_let(expr, level);                    break;
+		case NUMBER_NODE:
+			iprintfln(level, "NUMBER: %lf", NumNode_value(expr));
+			break;
+		case ID_NODE:
+			iprintfln(level, "ID: %s", IdNode_value(expr));
+			break;
+		case NEG_NODE:         NegNode_iprint(expr, level);                 break;
+		case APPLICATION_NODE: PairNode_iprint(expr, "APPLICATION", level); break;
+		case EXPT_NODE:        PairNode_iprint(expr, "EXPT", level);        break;
+		case PRODUCT_NODE:     PairNode_iprint(expr, "PRODUCT", level);     break;
+		case SUM_NODE:         PairNode_iprint(expr, "SUM", level);         break;
+		case CMP_NODE:         PairNode_iprint(expr, "CMP", level);         break;
+		case AND_NODE:         PairNode_iprint(expr, "AND", level);         break;
+		case OR_NODE:          PairNode_iprint(expr, "OR", level);          break;
+		case IF_NODE:          IfNode_iprint(expr, level);                  break;
+		case FN_NODE:          FnNode_iprint(expr, level);                  break;
+		case LET_NODE:         LetNode_iprint(expr, level);                 break;
 	}
 }
 
-void print_expr(const Node *expr)
+void Node_print(const Node *expr)
 {
-	print_tree(expr, 0);
+	Node_iprint(expr, 0);
 }
