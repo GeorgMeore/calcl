6e9016ba6f6d8041df9c73fdf620ffdcfb448068
GeorgII <georgii.galushko@gmail.com>
2023-09-18 22:15:53 +0300

    minor refactoring

--- env.h
+++ env.h
@@ -8,9 +8,9 @@ Env     *Env_new(Object *prev);
 void    Env_drop(Env *self);
 void    Env_add(Env *self, const char *key, Object *obj);
 Object  *Env_remove(Env *self, const char *key);
-int     Env_has(Env *self, const char *key);
-Object  *Env_get(Env *self, const char *key);
-void    Env_for_each(Env *self, void (*fn)(void *, Object *), void *param);
-void    Env_dump_objects(Env *self);
+int     Env_has(const Env *self, const char *key);
+Object  *Env_get(const Env *self, const char *key);
+void    Env_for_each(const Env *self, void (*fn)(void *, Object *), void *param);
+void    Env_dump_objects(const Env *self);
 
 #endif // HASH_INCLUDED
