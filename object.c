d6b835e9e06f81a93e2de308f298802d4f2b2797
GeorgII <>
2023-09-20 01:09:12 +0300

    add the `Object_print` procedure

--- object.c
+++ object.c
@@ -3,23 +3,29 @@
 #include <stdio.h>
 
 
-void Object_println(Object *obj)
+void Object_print(const Object *obj)
 {
 	switch (obj->type) {
 		case NUM_OBJECT:
-			printf("%lf\n", NumObj_num(obj));
+			printf("%lf", NumObj_num(obj));
 			return;
 		case FN_OBJECT:
-			printf("<fn %s>\n", FnObj_arg(obj));
+			printf("<fn %s>", FnObj_arg(obj));
 			return;
 		case ENV_OBJECT:
-			printf("<env-%p>\n", obj);
+			printf("<env-%p>", obj);
 			return;
 		case THUNK_OBJECT:
-			printf("<thunk-%p>\n", obj);
+			printf("<thunk-%p>", obj);
 			return;
 		case STACK_OBJECT:
-			printf("<stack-%p>\n", obj);
+			printf("<stack-%p>", obj);
 			return;
 	}
+}
+
+void Object_println(const Object *obj)
+{
+	Object_print(obj);
+	putchar('\n');
 }
